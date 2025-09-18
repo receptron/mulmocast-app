4b35cfe6d3d07eff64e702b4882c08f0c547738a
isamu <isamu@to-kyo.to>
2025-09-19 05:13:42 +0900

    fix type

--- src/renderer/composables/notify.ts
+++ src/renderer/composables/notify.ts
@@ -8,7 +8,7 @@ export const useApiErrorNotify = () => {
   const globalStore = useMulmoGlobalStore();
   const { t } = useI18n();
 
-  const apiErrorNotify = (keyName: string) => {
+  const apiErrorNotify = (keyName: keyof typeof ENV_KEYS) => {
     notifyError(t("ui.status.error"), t("notify.apiKey.error", { keyName: ENV_KEYS[keyName].title }), {
       label: t("notify.apiKey.setup"),
       onClick: () => globalStore.toggleSettingModal(),
