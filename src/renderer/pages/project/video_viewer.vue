fb3ab873b9bd345f24b5d1a86c0b1f74379b449f
isamu <isamu@to-kyo.to>
2025-09-23 12:36:22 +0900

    simple viewer

---
+++ src/renderer/pages/project/video_viewer.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="space-y-6">
+    <!-- Output Buttons -->
+    <MovieTab :project-id="projectId" />
+    <div class="space-y-4">
+      <div class="flex justify-center">
+        <Button
+          @click="generateContents"
+          class="mt-2 flex h-auto w-full items-center justify-center space-y-2 py-4 whitespace-normal"
+          data-testid="generate-contents-button"
+        >
+          <div class="mb-0 flex items-center justify-center gap-2">
+            <Monitor :size="24" />
+          </div>
+          <span>{{ t("project.generate.generateContents") }}</span>
+        </Button>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref, watch } from "vue";
+import { useMulmoEventStore } from "../../store";
+import { notifyProgress } from "@/lib/notification";
+import { FileText, Monitor, Volume2Icon } from "lucide-vue-next";
+import { Button } from "@/components/ui/button";
+import { getConcurrentTaskStatusMessageComponent } from "./concurrent_task_status_message";
+
+import MovieTab from "../../components/product/tabs/movie_tab.vue";
+
+import { useI18n } from "vue-i18n";
+
+interface Props {
+  projectId: string;
+}
+const props = defineProps<Props>();
+
+const { t } = useI18n();
+
+type OptionKey = "movie" | "audio" | "pdfSlide" | "pdfHandout";
+
+const options = ref<Record<OptionKey, boolean>>({
+  movie: true,
+  audio: true,
+  pdfSlide: false,
+  pdfHandout: false,
+});
+
+const generateContents = () => {
+  notifyProgress(window.electronAPI.mulmoHandler("mulmoActionRunner", props.projectId, ["movie"]), {
+    loadingMessage: ConcurrentTaskStatusMessageComponent,
+    successMessage: t("notify.content.successMessage"),
+    errorMessage: t("notify.content.errorMessage"),
+  });
+};
+const mulmoEventStore = useMulmoEventStore();
+
+const ConcurrentTaskStatusMessageComponent = getConcurrentTaskStatusMessageComponent(props.projectId);
+</script>
