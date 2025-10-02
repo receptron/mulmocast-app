808ac707326bb1f6c4a3ede2f48ae669a3f6895e
isamu <isamu@to-kyo.to>
2025-10-03 05:33:55 +0900

    0.0.10-rc-7

--- src/main/update.ts
+++ src/main/update.ts
@@ -1,4 +1,4 @@
-import type { MessageBoxOptions, autoUpdater, dialog } from "electron";
+import { type MessageBoxOptions, type autoUpdater, dialog } from "electron";
 
 
 import type { IUpdateDialogStrings, IUpdateInfo } from "update-electron-app";
