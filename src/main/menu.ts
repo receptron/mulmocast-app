87fa6d095b0ee494bbe6c0929c3e3684d92028e4
isamu <isamu@to-kyo.to>
2025-10-03 20:37:18 +0900

    update menu

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -1,4 +1,4 @@
-import { Menu, MenuItemConstructorOptions, BrowserWindow } from "electron";
+import { Menu, MenuItemConstructorOptions, BrowserWindow, shell } from "electron";
 
 const isDev = process.env.NODE_ENV === "development";
 
@@ -39,6 +39,20 @@ const template: MenuItemConstructorOptions[] = [
           if (win) {
             win.webContents.send("navigate", "/settings");
           }
+        },
+      },
+      {
+        label: "FAQ",
+        click: () => {
+          shell.openExternal("https://mulmocast.com/faq");
+        },
+      },
+      {
+        label: "ASK/Request",
+        click: () => {
+          shell.openExternal(
+            "https://docs.google.com/forms/d/e/1FAIpQLSdOOhskhHyjIUhVTeDJaaNcX04nWT_s7xJTRao3gnC9fT6xkA/viewform?usp=dialog",
+          );
         },
       },
     ],
