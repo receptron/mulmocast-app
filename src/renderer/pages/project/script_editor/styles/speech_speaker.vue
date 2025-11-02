827a97ec3aae7fdc463e6aa07b052fe4d7521020
Yasutaka Nishii <yasutaka.nishii@gmail.com>
2025-11-03 00:49:32 +0900

    add default volume for audio preview

--- src/renderer/pages/project/script_editor/styles/speech_speaker.vue
+++ src/renderer/pages/project/script_editor/styles/speech_speaker.vue
@@ -44,12 +44,14 @@
     <audio
       :src="`https://github.com/receptron/mulmocast-media/raw/refs/heads/main/voice/${localizedSpeaker.provider}/${localizedSpeaker.voiceId}.mp3`"
       controls
+      volume="0.3"
     />
   </div>
   <div v-if="localizedSpeaker.provider === 'nijivoice'">
     <audio
       :src="`https://github.com/receptron/mulmocast-media/raw/refs/heads/main/voice/${localizedSpeaker.provider}/${getVoiceList(localizedSpeaker.provider).find((a) => a.id === localizedSpeaker.voiceId).key}.mp3`"
       controls
+      volume="0.3"
     />
   </div>
   <div v-if="localizedSpeaker.provider === 'nijivoice'">
