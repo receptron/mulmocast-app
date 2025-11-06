99afeaec4dcd9c836726d58adf4a9d963dfeb1aa
isamu <isamu@to-kyo.to>
2025-11-07 06:31:34 +0900

    copy menu from menu-item-roles

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -4,13 +4,48 @@ import config from "../renderer/i18n/index";
 
 const isDev = process.env.NODE_ENV === "development";
 
+// https://github.com/electron/electron/blob/main/lib/browser/api/menu-item-roles.ts
 export const getMenu = async () => {
   const lang = settingsManager.loadAppLanguage();
   const menuProps = config.messages[lang as keyof typeof config.messages].nativeMenu;
+  const isMac = process.platform === "darwin";
 
   const template: MenuItemConstructorOptions[] = [
-    { role: "fileMenu", label: menuProps["fileMenu"] },
-    { role: "editMenu", label: menuProps["editMenu"] },
+    { role: "fileMenu", submenu: [isMac ? { role: "close" } : { role: "quit" }], label: menuProps["fileMenu"] },
+    {
+      role: "editMenu",
+      label: menuProps["editMenu"],
+      submenu: [
+        { role: "undo" },
+        { role: "redo" },
+        { type: "separator" },
+        { role: "cut" },
+        { role: "copy" },
+        { role: "paste" },
+        ...(isMac
+          ? ([
+              { role: "pasteAndMatchStyle" },
+              { role: "delete" },
+              { role: "selectAll" },
+              { type: "separator" },
+              {
+                label: "Substitutions",
+                submenu: [
+                  { role: "showSubstitutions" },
+                  { type: "separator" },
+                  { role: "toggleSmartQuotes" },
+                  { role: "toggleSmartDashes" },
+                  { role: "toggleTextReplacement" },
+                ],
+              },
+              {
+                label: "Speech",
+                submenu: [{ role: "startSpeaking" }, { role: "stopSpeaking" }],
+              },
+            ] as MenuItemConstructorOptions[])
+          : ([{ role: "delete" }, { type: "separator" }, { role: "selectAll" }] as MenuItemConstructorOptions[])),
+      ],
+    },
     {
       label: menuProps["view"],
       submenu: isDev
@@ -33,7 +68,17 @@ export const getMenu = async () => {
             { role: "togglefullscreen" },
           ],
     },
-    { role: "windowMenu", label: menuProps["windowMenu"] },
+    {
+      role: "windowMenu",
+      label: menuProps["windowMenu"],
+      submenu: [
+        { role: "minimize" },
+        { role: "zoom" },
+        ...(isMac
+          ? ([{ type: "separator" }, { role: "front" }] as MenuItemConstructorOptions[])
+          : ([{ role: "close" }] as MenuItemConstructorOptions[])),
+      ],
+    },
     {
       label: menuProps["help"],
       submenu: [
@@ -71,7 +116,7 @@ export const getMenu = async () => {
     },
   ];
 
-  if (process.platform === "darwin") template.unshift({ role: "appMenu" });
+  if (isMac) template.unshift({ role: "appMenu" });
 
   const menu = Menu.buildFromTemplate(template);
   return menu;
