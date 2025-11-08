a4c3862ad8b5ff97a63d325fbe7c803fe93045f2
isamu <isamu@to-kyo.to>
2025-11-08 14:54:31 +0900

    fix import

--- src/main/project_manager.ts
+++ src/main/project_manager.ts
@@ -8,7 +8,9 @@ import { GraphAILogger } from "graphai";
 
 import { Project, ProjectMetadata } from "../types";
 import { SCRIPT_EDITOR_TABS, MULMO_VIEWER_TABS } from "../shared/constants";
-import { initMulmoScript, onboardProjects } from "../shared/beat_data";
+import { initMulmoScript } from "../shared/beat_data";
+import { onboardProjects } from "../shared/onboard";
+
 // import { onboardMulmoScript }
 
 const PROJECTS_DIR = "projects";
